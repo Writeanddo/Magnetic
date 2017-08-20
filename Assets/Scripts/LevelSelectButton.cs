3d93fe322b4c377f8e02e794d1792aaf464ccac5
Nelson Laracuente <nlaracuente09@gmail.com>
2017-08-20 16:29:08 -0700

    updated the level select images and made it be smart to know which level to show based its name

--- Assets/Scripts/LevelSelectButton.cs
+++ Assets/Scripts/LevelSelectButton.cs
@@ -2,20 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Text.RegularExpressions;
 
 /// <summary>
-/// Represents a button selecting a level to play
+/// These are the buttons that take the player to a specific level
+/// The name of the button represents the level and scene to show/take the player to
+/// The component then updates it graphics based on the state (enabled/disabled)
+/// and the level number it is
 /// </summary>
 public class LevelSelectButton : MonoBehaviour
 {
     /// <summary>
-    /// The name of the scene associated with this level
+    /// A list containing the graphics that represents each level
+    /// Level counts starts at 1
+    /// Item 0 is reserved for the "locked" graphic
     /// </summary>
     [SerializeField]
-    string sceneName;
+    List<Sprite> buttonSprites;
+
+    /// <summary>
+    /// The name of the scene associated with this level
+    /// </summary>
     public string SceneName
     {
-        get { return this.sceneName; }
+        get { return this.name; }
+    }
+
+    /// <summary>
+    /// Extracts the level number from the scene name
+    /// </summary>
+    int levelNum = 0;
+    int LevelNum
+    {
+        get {
+
+            if(this.levelNum == 0) {
+                string num = Regex.Match(this.SceneName, @"\d+").Value;
+
+                if(!string.IsNullOrEmpty(num)) {
+                    this.levelNum = int.Parse(num);
+                }
+            }
+            
+            return this.levelNum;
+        }
     }
 
     /// <summary>
@@ -23,9 +53,20 @@ public class LevelSelectButton : MonoBehaviour
     /// </summary>
     void Awake()
     {
-        if(!GameManager.instance.IsLevelUnlocked(this.sceneName)) {
+        if(!GameManager.instance.IsLevelUnlocked(this.SceneName)) {
             GetComponent<Image>().enabled = false;
             GetComponent<Button>().interactable = false;
+            return;
         }
+
+        // Updates the image displayed ("Image.sprite") to show the level is available
+        // Updates the sprite states of this button to match the level number
+        SpriteState states = new SpriteState();
+        states.disabledSprite = this.buttonSprites[0];
+        states.highlightedSprite = this.buttonSprites[this.LevelNum];
+        states.pressedSprite = this.buttonSprites[this.LevelNum];
+
+        GetComponent<Image>().sprite = this.buttonSprites[this.LevelNum];
+        GetComponent<Button>().spriteState = states;
     }
 }
